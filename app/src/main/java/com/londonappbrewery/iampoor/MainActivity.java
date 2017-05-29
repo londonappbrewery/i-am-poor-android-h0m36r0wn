426554af979126e5ed8c3cad4147c12b36d38f02
h0m36r0wn <pepito.raviz@gmail.com>
2017-05-29 16:22:51 +0800

    "Challege Complete"

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -2,12 +2,18 @@ package com.londonappbrewery.iampoor;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String iAmPoor = "I am <b>poor</b>";
+        tv = (TextView) findViewById(R.id.textView);
+        tv.setText(Html.fromHtml(iAmPoor));
     }
 }
